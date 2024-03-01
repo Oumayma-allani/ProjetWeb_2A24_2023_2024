81540bed15ce37f1c1c6d7e1aae33fb798a88993
oumayma15 <oumayma.allani@esprit.tn>
2024-03-01 10:18:50 +0100

    third commit

---
+++ seance2/index.js
@@ -0,0 +1,21 @@
+function test()
+            {
+                alert('Hello A24');return false;
+            }
+            console.log("test");
+            let x=prompt("saisir votre nom","votre nom");
+            console.log(typeof(x));
+           let y= confirm(" quitter?");
+           console.log(typeof (y));
+
+            var a=5;
+            console.log("valeur a"+a);
+            var b=7;
+            console.log("valeur b"+b);
+            
+            
+            let d="test";
+            console.log(typeof (d));
+            var arr1=[5];
+            var arr2=Array(5);
+            
